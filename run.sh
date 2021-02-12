d0ff2cb9db9a6060e90df468e76e1f7e5c14c9c1
Alterac8 <awong189@calpoly.edu>
2021-02-12 10:38:06 -0800

    updated run.sh script able to run different programs by terminal parameters

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-export FLASK_APP=sample_backend.py
+if [[ $# -ne 1 ]]; then
+    echo "use: ./run.sh {app.py}"
+    exit 1
+fi
+
+export FLASK_APP=$1
 export FLASK_ENV=development
 flask run 

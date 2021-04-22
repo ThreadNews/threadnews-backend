acaf0b05a1542dd17405c8dd186ef8bb52b6a536
Alterac8 <awong189@calpoly.edu>
2021-04-22 00:02:07 -0700

    refactored scheduler

--- backend_vars.py
+++ backend_vars.py
@@ -2,6 +2,7 @@ import logger
 from config import threadConfiguration
 from database import threadDatabase
 from feed import NewsAPICalls
+from flask_apscheduler import APScheduler
 
 log = logger.setup_logger('root')
 configFile = threadConfiguration()
@@ -9,3 +10,4 @@ log.debug('initalized logger')
 
 appFeed = NewsAPICalls(configFile.get_configuration())
 database_client = threadDatabase(configFile.get_configuration())
+scheduler = APScheduler()

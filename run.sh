eea322fb197ec54f1d3b85b149c45c90063dc528
Alterac8 <awong189@calpoly.edu>
2021-04-25 14:35:41 -0700

    small changes to code

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-    echo "use: ./run.sh {app.py}"
+NO_RELOAD=""
+
+if [[ $# -lt 1 || $# -gt 2 ]]; then
+    echo "use: ./run.sh {app.py} [-r]"
     exit 1
+fi
+
+if [[ $# -eq 2 && $2 == "-r" ]]; then
+    NO_RELOAD="--no-reload"
 fi
 
 export FLASK_APP=$1
 export FLASK_ENV=development
-flask run 
+flask run $NO_RELOAD

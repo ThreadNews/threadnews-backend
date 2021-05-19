1dea66d36042e0cac0f22e085ad403f95289c011
Alterac8 <awong189@calpoly.edu>
2021-05-19 12:33:52 -0700

    format changes and allow travis to run all unit test

--- test/api_test.py
+++ test/api_test.py
@@ -6,8 +6,10 @@ from utils.config import threadConfiguration
 def config():
     return threadConfiguration()
 
+
 def test_collected_NewsAPI(config):
     assert config.get_api_keys() != None
+
 
 def test_has_env_vars(config):
     assert config.read_env_vars() != None
--- test/feed_test.py
+++ test/feed_test.py
@@ -7,10 +7,11 @@ from utils.config import threadConfiguration
 def newsFeed():
     return NewsAPI(threadConfiguration())
 
+
 def test_rotate(newsFeed):
-    if (isinstance(newsFeed.api_key, list)):
+    if isinstance(newsFeed.api_key, list):
         currentApi = newsFeed.api_key[0]
-        assert newsFeed.feed._rotate_api() == currentApi 
+        assert newsFeed.feed._rotate_api() == currentApi
         assert newsFeed.api_key[0] != currentApi
     else:
         currentApi = newsFeed.api_key

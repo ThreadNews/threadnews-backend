3d427022c4b82ed088270cb94228bbdc4622d73d
Alterac8 <awong189@calpoly.edu>
2021-06-07 15:54:44 -0700

    cleanup files and added a few more tests

--- endpoints/login.py
+++ endpoints/login.py
@@ -2,8 +2,7 @@ from flask import request, Blueprint
 import bcrypt
 from flask_jwt_extended import create_access_token
 from backend_vars import database_client, log
-import uuid
-import utils.db_templates
+import utils.database as db
 
 login_blueprint = Blueprint("login_blueprint", __name__)
 
@@ -58,7 +57,7 @@ def new_user():
 
         salt = bcrypt.gensalt()
         pass_hash = bcrypt.hashpw(str.encode(password), salt)
-        user = db_templates.user_template(user_name=username, email=email)
+        user = db.user_template(user_name=username, email=email)
         user["pass_hash"] = (pass_hash.decode(),)
 
         log.info("successfully parsed new user information")
